98bf22a60201dcabec0b51bddc6815ccd4140c7f
hamzeh hirzallah <hamzeh.hirzallah@gmail.com>
2017-09-21 00:35:25 +0300

    Init

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -1,9 +1,11 @@
 package com.londonappbrewery.xylophonepm;
 
+
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool mSonudPool;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,14 +34,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+        mSonudPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
 
 
         // TODO: Load and get the IDs to identify the sounds
-
+        mCSoundId = mSonudPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = mSonudPool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = mSonudPool.load(getApplicationContext(),R.raw.note3_e,1);
+        
 
     }
 
     // TODO: Add the play methods triggered by the buttons
+    public void playC(View v){
+        Log.d("xylophone","Key C is pressed");
+        mSonudPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+    }
+    public void playD(View v){
+        Log.d("xylophone","Key d is pressed");
+        mSonudPool.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+    }
+    public void playE(View v){
+        Log.d("xylophone","Key e is pressed");
+        mSonudPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,NO_LOOP,PRIORITY,NORMAL_PLAY_RATE);
+    }
 
 
 
